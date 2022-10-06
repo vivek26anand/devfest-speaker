872b2a56159c07abfe7e8265de1864a9badf7c57
vivek26anand <vivek26anand.va@gmail.com>
2022-10-06 05:43:52 +0530

    form page

--- src/From.js
+++ src/From.js
@@ -40,14 +40,14 @@ const states = [ "Andhra Pradesh",
 export default function Form(){
     return(
         <>
-        <div className="w-full z-20 bg-secondary relative md:py-10 py-5 md:h-screen">
+        <div className="w-full z-20 bg-secondary relative md:py-10 py-5">
             <div className="md:mx-10 mx-3 bg-white rounded-2xl md:px-16 px-4 py-8 border-2 border-secondary">
               <div className="flex justify-between">
-              <h1 className="md:text-5xl text-xl font-semibold font-sans text-tertiary">Register as a <span className="text-primary">Speaker</span></h1>
+              <h1 className="md:text-5xl text-2xl font-semibold font-sans text-tertiary">Register as a <span className="text-primary">Speaker</span></h1>
               <img src={Illustration2} alt="illustration" className='md:w-60 w-40'/>
               </div>
               <div>
-                <form className="flex md:flex-row flex-col" onSubmit={(e)=>{e.preventDefault()}}>
+                <form className="flex md:flex-row flex-col">
                     <div className='md:w-1/4'>
                     <label htmlFor="file-upload" className="text-sm font-medium text-tertiary flex flex-col items-center cursor-pointer">
                             Profile Photo
@@ -68,6 +68,10 @@ export default function Form(){
                     </div>
                     
                     <div className='flex flex-col md:w-3/4'> 
+                       <div className='text-lg text-tertiary font-bold'>
+                        Personal Information
+                        <hr className='w-full h-4 mt-1 border-t-2'/>
+                       </div>
                        {/* First Last and Middle Name */}
                        <div className='md:flex items-center justify-between'>
                         <div className='md:w-1/3 flex flex-col mb-4'>
@@ -120,10 +124,141 @@ export default function Form(){
                             </select>
                         </div>
                        </div>
+                        <div className='text-lg text-tertiary font-bold'>
+                            Socials
+                        <hr className='w-full h-4 mt-1 border-t-2'/>
+                        </div>
+
+                        {/* Social Links */}
                        
+                       <div className='md:flex items-center justify-between'>
+
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="twitter" className="text-tertiary font-semibold font-sans mb-2">Twitter</label>
+                            <input type="url" name="twitter" id="twitter" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="github" className="text-tertiary font-semibold font-sans mb-2">Github</label>
+                            <input type="url" name="github" id="github" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        
+                       </div>
+
+                       <div className='md:flex items-center justify-between'>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="linkedin" className="text-tertiary font-semibold font-sans mb-2">LinkedIn</label>
+                            <input type="url" name="linkedin" id="linkedin" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="website" className="text-tertiary font-semibold font-sans mb-2">Website</label>
+                            <input type="url" name="website" id="website" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                       </div>
+
+                       <div className='text-lg text-tertiary font-bold'>
+                            Professional Details
+                        <hr className='w-full h-4 mt-1 border-t-2'/>
+                        </div>
+
+                        {/* Professional */}
+                       
+                       <div className='md:flex items-center justify-between'>
+
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="organization" className="text-tertiary font-semibold font-sans mb-2">Organization</label>
+                            <input type="text" name="organization" id="organization" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="designation" className="text-tertiary font-semibold font-sans mb-2">Designation</label>
+                            <input type="text" name="designation" id="designation" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        
+                       </div>
+
+                       <div className='md:flex items-center justify-between'>
+
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="yearsofexperience" className="text-tertiary font-semibold font-sans mb-2">Years of experience</label>
+                            <input type="number" min={0} name="yearsofexperience" id="yearsofexperience" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="gde" className="text-tertiary font-semibold font-sans mb-2">GDE</label>
+                            <select className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary">
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
+                        </div>
+                        
+                       </div>
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="techstack" className="text-tertiary font-semibold font-sans mb-2">Tech Stack</label>
+                            <select className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary">
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
+                        </div>
+                   
+                        <div className='text-lg text-tertiary font-bold'>
+                            Session Details
+                        <hr className='w-full h-4 mt-1 border-t-2'/>
+                        </div>
+
+                        {/* Session */}
+                       
+                       <div className='md:flex items-center justify-between'>
+
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="topic" className="text-tertiary font-semibold font-sans mb-2">Topic</label>
+                            <input type="text" name="topic" id="topic" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="duration" className="text-tertiary font-semibold font-sans mb-2">Duration</label>
+                            <input type="number" name="duration" id="duration" className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                        
+                       </div>
+
+                       <div className='flex flex-col mb-4'>
+                            <label htmlFor="BriefofTopic" className="text-tertiary font-semibold font-sans mb-2">Brief of Topic</label>
+                            <textarea name="BriefofTopic" id="BriefofTopic" className="w-full border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary" required/>
+                        </div>
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="techstack" className="text-tertiary font-semibold font-sans mb-2">Tech Stack</label>
+                            <select className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary">
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
+                        </div>
+
+                        <div className='text-lg text-tertiary font-bold'>
+                        Preferences
+                        <hr className='w-full h-4 mt-1 border-t-2'/>
+                        </div>
+
+                        {/* Preferences */}
+
+                        <div className='md:flex items-center justify-between'>
+
+                       <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="food" className="text-tertiary font-semibold font-sans mb-2">Food</label>
+                            <select className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary">
+                                <option>VEG</option>
+                                <option>NON VEG</option>
+                            </select>
+                        </div>
+                        <div className='md:w-1/2 flex flex-col mb-4'>
+                            <label htmlFor="tshirt" className="text-tertiary font-semibold font-sans mb-2">T Shirt</label>
+                            <select className="md:w-5/6 border-2 border-tertiary rounded-md px-4 py-2 focus:border-primary focus:ring-primary">
+                                <option>S</option>
+                                <option>M</option>
+                                <option>L</option>
+                                <option>XL</option>
+                            </select>
+                        </div>
+                        
+                       </div>
+                       <input type="submit" value="Submit" className="mt-5 w-full bg-primary text-white font-bold py-2 px-4 rounded-md cursor-pointer"/>
                     </div>
-                    
-                    
                 </form>
               </div>
             </div>
